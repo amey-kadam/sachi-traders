ea0edaedd8836593f60337cdb801a94eed7d2b4d
Amey Kadam <97470463+amey-kadam@users.noreply.github.com>
2025-02-21 03:04:01 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,24 +1,37 @@
-// App.jsx
-import Navbar from './components/layout/Navbar'
-import Footer from './components/layout/Footer'
-import HeroSection from './components/sections/HeroSection'
-import Features from './components/sections/Features'
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import HeroSection from './components/sections/HeroSection';
+import Features from './components/sections/Features';
 import WhyChooseUs from "./components/sections/WhyChooseUs";
-import ClientsSection from './components/sections/clients'
+import ClientsSection from './components/sections/clients';
+import Products from './components/sections/Products.jsx';
+import About from './components/sections/About';
 
+
+const Home = () => (
+  <>
+    <HeroSection />
+    <Features />
+    <About/>
+    <WhyChooseUs />
+    <ClientsSection />
+  </>
+);
 
 const App = () => {
   return (
-    <div>
+    <>
       <Navbar />
-      <HeroSection />
-      <Features />
-      <WhyChooseUs/>
-      <ClientsSection/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* Use lowercase for path consistency */}
+        <Route path="/products" element={<Products />} />
+
+      </Routes>
       <Footer />
-    </div>
+    </>
   );
 };
 
 export default App;
-
